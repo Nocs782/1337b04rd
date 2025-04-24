fa57e073f659a28ff1034f1d305df8e95d8c724d
bambambim <dan4ik2006@mail.ru>
2025-04-24 18:12:54 +0500

    feat: models of comment, post, session

--- internal/domain/domain.go
+++ internal/domain/domain.go
@@ -1,1 +1,34 @@
 package domain
+
+import "time"
+
+type Post struct {
+	ID            int
+	Title         string
+	Content       string
+	AvatarURL     string
+	IMGsURLs      []string
+	Author        string
+	CreatedAt     time.Time
+	LastCommented time.Time
+	Deleted       bool
+}
+
+type Comment struct {
+	ID              int
+	PostID          int
+	ParentCommentID *int
+	AvatarURL       string
+	IMGsURLs        []string
+	Content         string
+	CreatedAt       time.Time
+	Author          string
+}
+
+type Session struct {
+	Name      string
+	IP        string
+	AvatarURL string
+	CreatedAt time.Time
+	ExpiresAt time.Time
+}
