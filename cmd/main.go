0c1ed99e48cfe7a8d80731f9b93e91de90f7c222
Nocs782 <emailforuniversity99@gmail.com>
2025-05-03 19:44:30 +0500

    feat: add for testing db and http server initialization

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"1337b04rd/internal/adapter/postgres"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+
+	"1337b04rd/internal/adapter/postgres"
+	"1337b04rd/internal/handler"
 )
 
 func main() {
 	dbURL := os.Getenv("DATABASE_URL")
+
 	if dbURL == "" {
 		log.Fatal("DATABASE_URL is not set.")
 	}
@@ -20,4 +24,15 @@ func main() {
 	defer db.Close()
 
 	fmt.Println("Successfully connected to the database.")
+
+	mux := http.NewServeMux()
+	handler.RegisterRoutes(mux, db)
+
+	port := os.Getenv("PORT")
+
+	fmt.Printf("Server is running on port %s\n", port)
+	err = http.ListenAndServe(":"+port, mux)
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
